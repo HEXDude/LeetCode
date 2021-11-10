5afb7905bddb91d682765238ecd04ed0abdf41cc
HEXDude <ZJakeZzz@Outlook.com>
2021-11-10 21:59:59 +0800

    🚧 删除有序数组中的重复项

--- ALGORITHM/EASY/REMOVE_DUPLICATES_FROM_SORTED_ARRAY.c
+++ ALGORITHM/EASY/REMOVE_DUPLICATES_FROM_SORTED_ARRAY.c
@@ -7,6 +7,17 @@
 #include <stdio.h>
 
 int removeDuplicates(int* nums, int numsSize){
+
+    if (numsSize == 0)
+    {
+        return 0;
+    }
+
+    if (numsSize == 1)
+    {
+        return 1;
+    }
+
     int init = -1;
     int numsSizeCopy = 0;
     for (int i = 0; i < numsSize; i++)
@@ -39,11 +50,11 @@ int removeDuplicatesPoniter(int* nums, int numsSize){
     int faster = 1, slower = 1;
     
     /**
-     * 定义快慢指针从1开始遍历
-     * 如果快指针遍历的数不等于前面的数‘
-     * 则说明此升序数组已遍历的值不重复
-     * 则将慢指针移到快指针位置
-     * 如果等于，则说明此处发生了连续，则将
+     * 快慢指针
+     * 如果不发生连续，慢指针的值为当前快指针指向的值
+     * 该步比较重要，因为如果快慢指针的值相等时进行
+     * 交换无所谓。但是快指针经历了一连串连续的值后
+     * 可以对慢指针指向的值进行更新
      */
     while (faster < numsSize)
     {
