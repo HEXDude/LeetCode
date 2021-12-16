6cd9fd2e66a497d4886ab96de265631af52b3a32
HEXDude <ZJakeZzz@Outlook.com>
2021-12-16 09:52:33 +0800

    🚧 降级CMake版本，提交寻找最长回文数的模板

---
+++ Algorithms/Longest-Palindromic-Substring.c
@@ -0,0 +1,25 @@
+/**
+ * @author HEXDude
+ * @date 2021/12/16
+ * @description 寻找最长回文数，如果字符串长度为一则是它本身
+ * @Reference https://leetcode-cn.com/problems/longest-palindromic-substring/
+ * TODO HEXDude 最长回文数
+ */
+
+/**
+* Local part
+*/
+#include <stdio.h>
+
+int main() {
+    printf("1");
+}
+
+/**
+* Submit part
+*/
+char * longestPalindrome(char * s){
+    if (sizeof(s) <= 1) {
+        return s;
+    }
+}
