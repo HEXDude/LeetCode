6dc423eb2bf53a19c3d33de57b31eb41f52f4699
HEXDude <ZJakeZzz@Outlook.com>
2021-12-23 15:07:25 +0800

    🚧 算法 167 两数之和-Ⅱ-输入有序数组

---
+++ Algorithms/167-Two-Sum-II-Input-Array-Is-Sorted.c
@@ -0,0 +1,28 @@
+/**
+ * @author HEXDude
+ * @date 2021/12/18
+ * @description 给定一个已按照非递减顺序排序的整数数组numbers ，请你从数组中找出两个数满足相加之和等于目标数 target 。
+ * 函数应该以长度为 2 的整数数组的形式返回这两个数的下标值。numbers 的下标 从 1 开始计数 ，所以答案数组应当满足 1 <= answer[0] < answer[1] <= numbers.length 。
+ * 你可以假设每个输入 只对应唯一的答案 ，而且你 不可以 重复使用相同的元素。
+ * @reference https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted
+ * @conclusion
+ */
+
+/**
+ * Local Part
+ */
+#include <stdio.h>
+#include <malloc.h>
+int* twoSum(int* numbers, int numbersSize, int target, int* returnSize);
+
+int main() {
+
+}
+
+/**
+ * Submit part
+ * The return array must be malloced and assume its calls called free.
+ */
+int* twoSum(int* numbers, int numbersSize, int target, int* returnSize){
+
+}
