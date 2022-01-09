88e63a572ca5aae18fd71ec9838e3a6d1c2b84be
HEXDude <ZJakeZzz@Outlook.com>
2022-01-09 10:46:01 +0800

    💩 优化

--- Algorithms/879-Middle-Of-The-Linked-List.c
+++ Algorithms/879-Middle-Of-The-Linked-List.c
@@ -4,7 +4,6 @@
  * @description 给定一个头结点为 head 的非空单链表，返回链表的中间结点。如果有两个中间结点，则返回第二个中间结点。
  * @reference https://leetcode-cn.com/problems/middle-of-the-linked-list/
  * @conclusion 双指针（快慢指针）， 快指针每次走两步而慢指针每次走一步，这样在快指针到达尽头时，慢指针一定是在中间位置的。
- * TODO HEXDude Need fix
  */
 
 //Local Part Start
