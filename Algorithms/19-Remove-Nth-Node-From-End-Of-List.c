89cb3cd5c7601b52f3a87a669fac5d8d9f68c266
HEXDude <ZJakeZzz@Outlook.com>
2022-01-09 11:09:24 +0800

    💩 优化

--- Algorithms/19-Remove-Nth-Node-From-End-Of-List.c
+++ Algorithms/19-Remove-Nth-Node-From-End-Of-List.c
@@ -3,10 +3,23 @@
  * @date 12/29/21
  * @description 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
  * @reference https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/
- * @conclusion TODO HEXDude Need fix
+ * @conclusion 主要难点在于链表你位置长度，所以需要先获取长度后再找到要删除的节点后，截再将被删除节点的父节点指向
+ * 被删除节点的子节点。
  */
 
 //Local Part Start
+#include <stdio.h>
+#include <malloc.h>
+
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n);
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+int main() {
+
+}
 //Local Part End
 
 //Submit Part Start
