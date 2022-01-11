ea7c2218683e153ba85c026dc925afe9cb0bfa16
HEXDude <ZJakeZzz@Outlook.com>
2022-01-11 16:20:15 +0800

    :construction: 算法 567 字符串的排列

--- Algorithms/567-Permutation-In-String.c
+++ Algorithms/567-Permutation-In-String.c
@@ -3,7 +3,9 @@
  * @date 2022/1/11
  * @description 给你两个字符串s1和s2 ，写一个函数来判断 s2 是否包含 s1的排列。如果是，返回 true ；否则，返回 false 。换句话说，s1 的排列之一是 s2 的 子串 。
  * @reference https://leetcode-cn.com/problems/permutation-in-string/
- * @conclusion
+ * @conclusion 题目好难理解，翻译一下应该是有s1,s2两个字符串，s2中可能存在一个连续的字串与s1一致，
+ * 但是顺序不一样。Hash不适合这种写入，麻烦。可以分别为两个字符串根据ASCII码排序检索，如果出现则累加。
+ * 最终在S2中进行滑窗，如果滑窗的区间值与s1的检索值一样，则true。
  */
 
 /*
@@ -17,8 +19,8 @@ bool checkInclusion(char * s1, char * s2);
 bool equals(int * nums1, int * nums2);
 
 int main() {
-    char * s1 = "ab";
-    char * s2 = "eidbaooo";
+    char * s1 = "adc";
+    char * s2 = "dcda";
     checkInclusion(s1, s2);
 }
 /*
@@ -36,18 +38,17 @@ bool checkInclusion(char* s1, char* s2) {
     /*
      * 建立对于S1中字符出现次数的统计数组
      */
-    int cnt1[26];
+    int cnt1[26], cnt2[26];
     memset(cnt1, 0, sizeof(cnt1));
     for (int i = 0; i < n; i++) {
         cnt1[s1[i] - 'a']++;
     }
 
-    for (int i = 0; i < m-n; ++i) {
-        int cnt2[26];
+    for (int i = n-1 ; i < m; ++i) {
         memset(cnt2, 0, sizeof(cnt2));
-        for (int j = 0; j < n; j++) {
-            char x = s2[i+j];
-            cnt2[s2[i+j] - 'a']++;
+        for (int j = n - 1; j >= 0; j--) {
+            char x = s2[i-j];
+            cnt2[s2[i-j] - 'a']++;
         }
         if (equals(cnt1, cnt2)) {
             return true;
