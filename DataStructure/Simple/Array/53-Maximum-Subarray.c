6ae16319391736a59d73e788c7663bd285dd96c6
HEXDude <HEXDude@ICloud.Com>
2022-04-23 15:14:57 +0800

    优化结构 数据结构 简单 数组 53 最大子数组和

--- DataStructure/Simple/Array/53-Maximum-Subarray.c
+++ DataStructure/Simple/Array/53-Maximum-Subarray.c
@@ -1,68 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <malloc.h>
+#include <math.h>
+#include <C:\Users\HEXDude\WorkSpace\LeetCode\Library\uthash.h>
+
 /**
  * @author HEXDude
  * @date 2022/4/18
- * @description 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
- * @reference https://leetcode-cn.com/problems/maximum-subarray/
- * @conclusion 滚动数组进行查询，每次遍历比较之前的值与当前遍历值的大小，取大的作为结果，最佳算法时间复杂度O(n)
+ * @description 给你一个整数数组nums,请你找出一个具有最大和的连续子数组(子数组最少包含一个元素)，返回其最大和。
+ * @link https://leetcode-cn.com/problems/maximum-subarray/
+ * @conclusion
  */
-int maxSubArray_OlogN(int *nums, int numsSize);
-int maxSubArray_On(int* nums, int numsSize);
+
+int maxSubArray(int *nums, int numsSize);
+
 int compare(int x, int y);
 
 int main() {
-    int nums[10] = {-2,1,-3,4,-1,2,1,-5,4};
-    int x = maxSubArray_OlogN(nums, 10);
-    int y = maxSubArray_On(nums, 10);
+	int nums[10] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+	int result = maxSubArray(nums, sizeof(nums) / sizeof(int));
+}
+
+int maxSubArray(int *nums, int numsSize) {
+	/**
+	 * 分别定义一个遍历时的临时最大值,用来记录每次遍历时获得的最大值
+	 * 和一个全局最大值用来记录整个数组遍历结束后的最大值
+	 * 全局最大值取数组中的第一个元素,以防止最大值为负数的情况
+	 */
+	int tempMax = 0, globalMax = nums[0];
+
+	for (int i = 0; i < numsSize; ++i) {
+		//比较当前遍历的数与临时最大值大小,并保持临时最大值为较大值
+		tempMax = compare(nums[i], tempMax + nums[i]);
+		//比较临时变量值和全局最大值,并保持全局最大值为较大值
+		globalMax = compare(tempMax, globalMax);
+	}
+
+	return globalMax;
 }
 
 /**
- * 设置两个临时变量：全局最大值、区域最大值
- * 遍历原始数组，当区域最大值+当前遍历元素>=当前遍历元素时，则说明此段为目前最大连续
- * 否则，将此区域连续最大值与全局连续最大值作比较，如果大于全局最大值则将区域最大值赋
- * 值给全局最大值，并将区域最大值丢弃采用当前遍历元素作为初始值。
- * 此方法时间复杂度略长O(logN)
- * @param nums
- * @param numsSize
- * @return
+ * 比较给定的两个数,任意一个数大于等于另一个数则返回大于等于的数,否则返回另一个.
+ * @param x int
+ * @param y int
+ * @return MAX(x,y)
  */
-int maxSubArray_OlogN(int* nums, int numsSize) {
-    int globalMaxSum = nums[0];
-    int areaMaxSum = nums[0];
-
-    for (int i = 1; i < numsSize; i++) {
-        if (areaMaxSum + nums[i] >= nums[i]) {
-            areaMaxSum += nums[i];
-        } else {
-            areaMaxSum = nums[i];
-        }
-
-        if (areaMaxSum > globalMaxSum) {
-            globalMaxSum = areaMaxSum;
-        }
-    }
-
-    return globalMaxSum;
-}
-
-/**
- * 优化了上面O(logN)的算法，基本思路一致但是改为滚动数组，时间复杂度降低。
- * @param nums
- * @param numsSize
- * @return
- */
-int maxSubArray_On(int* nums, int numsSize) {
-    int tempResult = 0, maxSum = nums[0];
-    for (int i = 0; i < numsSize; ++i) {
-        tempResult = compare(tempResult + nums[i], nums[i]);
-        maxSum = compare(tempResult, maxSum);
-    }
-    return maxSum;
-}
-
 int compare(int x, int y) {
-    if (x >= y) {
-        return x;
-    } else {
-        return y;
-    }
+	if (x >= y) {
+		return x;
+	} else {
+		return y;
+	}
 }
