3169ab02a11802e204b3992c1f6d552c82dc7214
HEXDude <HEXDude@ICloud.Com>
2022-04-23 17:30:53 +0800

    优化结构 数据结构 简单 数组 121 买卖股票的最佳时机

--- DataStructure/Simple/Array/121-Best-Time-To-Buy-And-sell-Stock.c
+++ DataStructure/Simple/Array/121-Best-Time-To-Buy-And-sell-Stock.c
@@ -1,33 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <malloc.h>
+#include <math.h>
+#include <C:\Users\HEXDude\WorkSpace\LeetCode\Library\uthash.h>
+
 /**
  * @author HEXDude
- * @date ${date}
- * @description 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
- * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
- * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
- * @reference https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
+ * @date 2022/4/20
+ * @description 给定一个数组prices,它的第i个元素prices[i]表示一支给定股票第i天的价格.
+ * 你只能选择某一天买入这只股票,并选择在未来的某一个不同的日子卖出该股票.设计一个算法来计算你所能获取的最大利润.
+ * @link https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
  * @conclusion
  */
-#include <stdlib.h>
-
-int maxProfit(int* prices, int pricesSize);
+int maxProfit(int *prices, int pricesSize);
 
 int main() {
-    int nums1[6] = {7,1,5,3,6,4};
-    int nums2[5] = {7,6,4,3,1};
-	int nums3[3] = {1, 4, 2};
-    int nums1Size = 6, nums2Size = 5, nums3Size=  3;
-	int x = maxProfit(nums3, nums3Size);
+	int nums[6] = {7, 1, 5, 3, 6, 4};
+	int result = maxProfit(nums, 6);
 }
 
-int maxProfit(int* prices, int pricesSize) {
-	int max = 10000;
+int maxProfit(int *prices, int pricesSize) {
+	//由题意可得最大买入卖出价格为10000
+	int maxPrice = 10000;
+	//不可能亏本,预设利润为零
 	int maxProfit = 0;
 
 	for (int i = 0; i < pricesSize; ++i) {
-		if (prices[i] < max) {
-			max = prices[i];
-		} else if (prices[i] - max > maxProfit) {
-			maxProfit = prices[i] - max;
+		if (prices[i] < maxPrice) {
+			maxPrice = prices[i];
+		} else if (prices[i] - maxPrice > maxProfit) {
+			maxProfit = prices[i] - maxPrice;
 		}
 	}
 
