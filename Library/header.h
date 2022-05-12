75bed2100e3c70a225234443db14ce700f8eca07
HEXDude <HEXDude@ICloud.Com>
2022-05-12 11:05:57 +0800

    新增自定义全局头文件

---
+++ Library/header.h
@@ -0,0 +1,16 @@
+/**
+ * @author HEXDude
+ * @date 2022/5/12
+ * @description 自定义全局Header文件
+ * @reference
+ * @conclusion
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <malloc.h>
+#include <math.h>
+#include <string.h>
+#include <limits.h>
+#include "uthash.h"
